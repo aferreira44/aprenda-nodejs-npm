2a20ef00904b56f7381ed22d84ab840faa1d9808
André Ferreira <contato@andreferreira.me>
2017-05-31 17:36:24 -0300

    feat: Add http module exercises

---
+++ exemplos/http/http.js
@@ -0,0 +1,25 @@
+var router = require('./router');
+
+var app = router(3412);
+
+var operadoras = [
+    {nome: "Oi", codigo: 14, categoria: "Celular", preco: 2},
+    {nome: "Vivo", codigo: 15, categoria: "Celular", preco: 1},
+    {nome: "Tim", codigo: 45, categoria: "Celular", preco: 3}
+];
+
+var contatos = [
+    {id: 1, nome: "André", telefone: '9999-1111', operadora: operadoras[0], data: new Date()},
+    {id: 2, nome: "Sandra", telefone: '9999-2222', operadora: operadoras[1], data: new Date()},
+    {id: 3, nome: "Mariana", telefone: '9999-3333', operadora: operadoras[2], data: new Date()}
+];
+
+app.get('/operadoras', function (req, res) {
+    res.write(JSON.stringify(operadoras));
+    res.end();
+});
+
+app.get('/contatos', function (req, res) {
+    res.write(JSON.stringify(contatos));
+    res.end();
+});
